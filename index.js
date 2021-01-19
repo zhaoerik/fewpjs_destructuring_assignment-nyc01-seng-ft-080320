2e1e7e585b37a7d956e7913dab19c8218a99e7b5
zhaoerik <jediofthenyy@gmail.com>
2021-01-19 17:33:18 -0500

    done

--- index.js
+++ index.js
@@ -31,10 +31,10 @@ const k_muppet = {
 const [moo, neigh, baa, oink, cluck] = 'cow horse sheep pig chicken'.split(' ')
 
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them Bessie, Dolly, Babe, and Little.
-const [Bessie, ,Dolly, Babe, Little] = 'cow horse sheep pig chicken'.split(' ')
+const [Bessie, , Dolly, Babe, Little] = 'cow horse sheep pig chicken'.split(' ')
 
 // 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of black_and_white, black, and pink.
-const [black_and_white, ,black, pink, ] = 'cow horse sheep pig chicken'.split(' ')
+const [black_and_white, , black, pink, ] = 'cow horse sheep pig chicken'.split(' ')
 
 // Arrays
 
